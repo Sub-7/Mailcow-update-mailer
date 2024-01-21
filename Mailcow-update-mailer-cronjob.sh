621bc54623d5fc69e04180dee312be4272b8dc9b
Sub7 <30667941+Sub-7@users.noreply.github.com>
2024-01-21 15:22:48 +0100

    Create Mailcow-update-mailer-cronjob.sh

---
+++ Mailcow-update-mailer-cronjob.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Shell script for a daily cron job
+
+# Function to validate the time
+validate_time() {
+  local time_pattern="^([01]?[0-9]|2[0-3]):[0-5][0-9]$"
+  if [[ $1 =~ $time_pattern ]]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+# Set default values
+default_execution_time="03:00"
+
+# Ask for the time of day for execution with default value
+read -p "Enter the time for daily execution in HH:MM format [default: $default_execution_time]: " selected_time
+selected_time="${selected_time:-$default_execution_time}"
+# Validate the time
+while ! validate_time "$selected_time"; do
+  read -p "Invalid input. Please enter the time in HH:MM format: " selected_time
+done
+
+# Create cron job entry
+cron_entry="$selected_time * * * * /root/Mailcow-update-mailer.sh"
+
+# Add cron job
+echo "Cron job will be added:"
+echo "$cron_entry"
+(crontab -l ; echo "$cron_entry") | crontab -
+
+echo "Cron job successfully added."
